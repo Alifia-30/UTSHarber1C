fa4ba907ff22d978ae8c19da62f81e237cc4aeb0
ashabrina600@gmail.com <ashabrina600@gmail.com>
2023-11-03 11:34:57 +0700

    penambahan modul test.java

---
+++ src/tes.java
@@ -0,0 +1,19 @@
+public class tes {
+    public static void main(String[] args) {
+        int angka1 = 30;
+        int angka2 = 12;
+
+        int hasil = angka1 + angka2;
+
+        System.out.println("hasilnya adalah" + hasil);
+        //commit di git ini digunakan untuk menyetujui
+        //perubahan pada suatu coding
+        // jadi misalnya di coding ada beberapa penambahan,
+        // sebelum diupload ke github harus dicommit dulu
+        //commit juga berguna untuk berkerja secara kolaborasi
+        //misalnya dalam suatu project terdapat beberapa programmer
+        //commit diapprove oleh pemimpin project untuk nantinya digabungan
+        //dengan code dari developer lain dalam satu github project
+
+    }
+}
